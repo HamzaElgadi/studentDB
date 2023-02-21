fdda67dc7d9049e860ad6ce2600bf0bd6dbd0a43
Vishal Shende <vishal.shende@gmail.com>
2023-02-21 11:31:46 -0500

    Updated

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.endpoint = ''
-config.key = ''
+config.endpoint = 'https://vshende.documents.azure.com:443/'
+config.key = 'jlkiXtCtoQvavPwgWnMiyH9I5aPBxYmU0eVWkEkp4D0RhvWvcMplvGykKxJWVUzm2dcnHqNwDT7kACDbzoOjcA=='
 
 config.database = { id: 'studentdb' }
 config.container = { id: 'students' }
