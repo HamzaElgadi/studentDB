8a7425c8e72850997dda40f892baa3445d376ecf
Elgadi Hamza <elgadi.hamza2@gmail.com>
2024-10-12 01:23:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config = {}
 config.endpoint = 'https://memoriesdb.documents.azure.com:443/'
 config.key = 'jlkiXtCtoQvavPwgWnMiyH9I5aPBxYmU0eVWkEkp4D0RhvWvcMplvGykKxJWVUzm2dcnHqNwDT7kACDbzoOjcA=='
 
-config.database = { id: 'title' }
+config.database = { id: 'CloudMemories' }
 config.container = { id: 'content' }
 
 module.exports = config;
